a25eaa259fe024df0f3161bc18c6a6d144c42159
smithatech <smitha.sreekantan@gmail.com>
2026-01-18 22:49:31 +0530

    prod-deploy.yml with prod details

--- src/main/java/com/saikvt/event/entity/Questionnaire.java
+++
@@ -1,5 +0,0 @@
-package com.saikvt.event.entity;
-
-// This file was intentionally removed from the codebase.
-// The original Questionnaire POJO has been deleted to prevent JPA/entity scanning.
-// If you need the original class, restore it from version control history.
